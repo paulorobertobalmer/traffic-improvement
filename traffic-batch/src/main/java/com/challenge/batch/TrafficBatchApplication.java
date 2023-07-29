bae43a2f8cccad6fdf62231b704e387bd42bf590
Paulo Roberto Balmer <paulo@balmersistemas.com.br>
2023-07-29 14:05:09 +0100

    create common project with entities to use in both projects

--- traffic-batch/src/main/java/com/challenge/batch/TrafficBatchApplication.java
+++ traffic-batch/src/main/java/com/challenge/batch/TrafficBatchApplication.java
@@ -1,13 +1,33 @@
 package com.challenge.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 
 @SpringBootApplication
 public class TrafficBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrafficBatchApplication.class, args);
+	}
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job csvJob;
+
+	public void run(String... args) throws Exception {
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addLong("time", System.currentTimeMillis())
+				.toJobParameters();
+
+		jobLauncher.run(csvJob, jobParameters);
 	}
 
 }
---
+++ traffic-common/src/main/java/com/challenge/common/entity/Location.java
@@ -0,0 +1,22 @@
+package com.challenge.common.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "location")
+public record Location(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Integer id,
+        String borough,
+        String zoneName,
+        String serviceZone,
+        Timestamp createdAt
+) {
+
+}
